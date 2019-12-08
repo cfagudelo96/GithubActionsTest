314124abe397438c0c2713abe2a021797fbf4518
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2019-12-08 14:23:19 -0500

    Set up action

---
+++ README.md
@@ -0,0 +1,1 @@
+![](https://github.com/cfagudelo96/GithubActionsTest/workflows/Continuous%20integration%20workflow/badge.svg)
--- github-actions-test-back-end/src/app.service.ts
+++ github-actions-test-back-end/src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getGood(): string {
+    return 'Git Gud';
+  }
+
+  getGoodBye(): string {
+    return 'Bye';
+  }
 }
